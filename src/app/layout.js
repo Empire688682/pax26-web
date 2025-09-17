cdd03226bc4547a4cba4257613dd5c2dd6429613
juwon <asehindej@gmail.com>
2025-09-18 00:46:29 +0100

    auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -49,6 +49,7 @@ export default function RootLayout({ children }) {
           {children}
         </ClientWrapper>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
