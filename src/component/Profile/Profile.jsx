d90d281b7dd80cbb39a2a4b29a659d0fc347ed34
juwon <asehindej@gmail.com>
2025-09-28 23:53:59 +0100

    wfer

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -14,6 +14,52 @@ const Profile = () => {
   const { userData, setUserData, pax26, logoutUser, setPinModal, transactionHistory, getUserRealTimeData } = useGlobalContext();
   const [userImage, setUserImage] = useState(null);
   const [processing, setProcessing] = useState(false);
+  const [bvn, setBvn] = useState("");
+  const [userBvn, setUserBvn] = useState(false);
+
+  // add another loading state for BVN
+  const [bvnLoading, setBvnLoading] = useState(false);
+
+  const verifyBvn = async (e) => {
+    e.preventDefault();
+
+    if (bvn.length !== 11) {
+      toast.error("BVN must be exactly 11 digits");
+      return;
+    }
+
+    setBvnLoading(true);
+    try {
+      const response = await axios.get(
+        `https://api.flutterwave.com/v3/kyc/bvns/${bvn}`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_FLW_SECRET_KEY}`,
+            // 👆 safer to expose prefixed env var in frontend
+          },
+        }
+      );
+
+      if (response.data?.status === "success") {
+        toast.success("BVN verified successfully ✅");
+
+        // save BVN verified flag in user profile if needed
+        // await axios.post("/api/update-bvn", { bvn });
+
+        setUserBvn(false);
+      } else {
+        toast.error("Failed to verify BVN ❌");
+      }
+
+      console.log("BVN Verification response:", response.data);
+    } catch (error) {
+      console.error("BVN Verification Error:", error.response?.data || error.message);
+      toast.error("Error verifying BVN. Try again.");
+    } finally {
+      setBvnLoading(false);
+    }
+  };
+
 
   const updateUserProfileImg = async () => {
     setProcessing(true);
@@ -25,13 +71,13 @@ const Profile = () => {
       return;
     }
     try {
-      const response = await axios.post("/api/update-profileImage", {profileImage});
+      const response = await axios.post("/api/update-profileImage", { profileImage });
       console.log("response:", response);
       if (response.data.success) {
-        if(typeof window !== "undefined"){
+        if (typeof window !== "undefined") {
           const savedData = localStorage.getItem("userData");
-          
-          if(!savedData){
+
+          if (!savedData) {
             toast.error("An error occured try again");
             return
           }
@@ -71,41 +117,41 @@ const Profile = () => {
 
   return (
     <div className="min-h-screen py-12 px-6"
-    style={{backgroundColor:pax26.secondaryBg}}>
+      style={{ backgroundColor: pax26.secondaryBg }}>
       <ToastContainer />
       <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-6">
         { /* Left: Profile Overview */}
         <div className="backdrop-blur-md p-6 rounded-2xl shadow-xl flex flex-col items-center text-center"
-        style={{backgroundColor:pax26.bg}}>
+          style={{ backgroundColor: pax26.bg }}>
           <div className="relative w-18 h-18 rounded-full overflow-hidden shadow-lg mb-2">
             <Image
-              src={userImage? window.URL.createObjectURL(userImage) : userData.profileImage || "/profile-img.png"}
+              src={userImage ? window.URL.createObjectURL(userImage) : userData.profileImage || "/profile-img.png"}
               alt="Profile"
               fill
               style={{ objectFit: "cover" }}
             />
             {
-              !userImage && <label 
-              htmlFor="profileImage"
-              className='absolute bottom-[-3px] bg-gray-100 rounded-full left-1/2 -translate-x-1/2 cursor-pointer'
+              !userImage && <label
+                htmlFor="profileImage"
+                className='absolute bottom-[-3px] bg-gray-100 rounded-full left-1/2 -translate-x-1/2 cursor-pointer'
               >
                 <CameraIcon size={30} />
               </label>
             }
           </div>
           <div>
-            <input 
-            type="file" 
-            hidden 
-            name="profileImage" 
-            id="profileImage"
-            accept='image/*' 
-            onChange={(e) => setUserImage(e.target.files[0])} />
+            <input
+              type="file"
+              hidden
+              name="profileImage"
+              id="profileImage"
+              accept='image/*'
+              onChange={(e) => setUserImage(e.target.files[0])} />
             {
               userImage && !processing && <div>
-                <p 
-                onClick={updateUserProfileImg}
-                className="bottom-0 mx-auto w-[100px] bg-white border-1 p-1 rounded-sm text-xs cursor-pointer text-gray-900">
+                <p
+                  onClick={updateUserProfileImg}
+                  className="bottom-0 mx-auto w-[100px] bg-white border-1 p-1 rounded-sm text-xs cursor-pointer text-gray-900">
                   Save</p>
               </div>
             }
@@ -125,20 +171,63 @@ const Profile = () => {
               <span className="text-green-600 flex items-center justify-center gap-2 text-sm">
                 <ShieldCheck size={16} /> BVN Verified
               </span>
-            ) : (
-              <div className="text-red-500 flex flex-col items-center gap-1 text-sm">
-                <span className="flex items-center gap-1">
-                  <ShieldAlert size={16} /> BVN Not Verified
-                </span>
-                <button
-                  onClick={() => toast.info('Comming soon...')}
-                  className="bg-yellow-400 mt-2 px-3 py-1 rounded-md text-xs text-black hover:bg-yellow-500"
-                >
-                  Verify Now
-                </button>
-              </div>
-            )}
+            ) : <>
+              {
+                !userBvn ? <div className="text-red-500 flex flex-col items-center gap-1 text-sm">
+                  <span className="flex items-center gap-1">
+                    <ShieldAlert size={16} /> BVN Not Verified
+                  </span>
+                  <button
+                    onClick={() => setUserBvn(true)}
+                    className="bg-yellow-400 mt-2 px-3 py-1 rounded-md text-xs text-black hover:bg-yellow-500"
+                  >
+                    Verify Now
+                  </button>
+                </div>:null
+              }
+            </>}
           </div>
+
+          {userBvn && (
+            <form
+              onSubmit={verifyBvn}
+              style={{ backgroundColor: pax26.secondaryBg }}
+              className="w-full max-w-md mx-auto shadow-md rounded-lg p-6"
+            >
+              <label className="block text-sm font-medium mb-2"
+                style={{ color: pax26.textPrimary }}>
+                Bank Verification Number (BVN)
+              </label>
+
+              <input
+                type="text"
+                value={bvn}
+                onChange={(e) => {
+                  const val = e.target.value.replace(/\D/g, ""); // only numbers
+                  if (val.length <= 11) setBvn(val);
+                }}
+                maxLength={11}
+                placeholder="Enter your 11-digit BVN"
+                className="w-full px-4 py-2 border-2 border-gray-300 rounded-lg 
+                 focus:outline-none focus:ring-2 focus:ring-blue-500 
+                 focus:border-blue-500 transition"
+                style={{ color: pax26.textPrimary }}
+              />
+
+              {bvn && bvn.length < 11 && (
+                <p className="text-red-500 text-xs mt-1">BVN must be 11 digits</p>
+              )}
+
+              <button
+                type="submit"
+                disabled={bvnLoading}
+                className="mt-4 w-full bg-blue-600 text-white py-2 px-4 rounded-lg
+                 hover:bg-blue-700 disabled:opacity-50 transition"
+              >
+                {bvnLoading ? "Verifying..." : "Verify BVN"}
+              </button>
+            </form>
+          )}
 
           <div className="mt-6 space-y-3 w-full">
             <button
@@ -158,15 +247,15 @@ const Profile = () => {
         </div>
 
         {/* Middle: Settings & Change Password */}
-        <div 
-        style={{backgroundColor:pax26.bg}}
-        className="p-6 rounded-2xl shadow-xl space-y-8">
+        <div
+          style={{ backgroundColor: pax26.bg }}
+          className="p-6 rounded-2xl shadow-xl space-y-8">
           <div>
             <h3 className="text-lg font-semibold text-gray-400 border-b pb-2 mb-4"
-            style={{color:pax26.textPrimary}}>Settings</h3>
+              style={{ color: pax26.textPrimary }}>Settings</h3>
             <div className="flex items-center justify-between mb-4">
               <span className="flex items-center gap-2 text-sm"
-              style={{color:pax26.textSecondary}}><Bell size={18} /> Notifications</span>
+                style={{ color: pax26.textSecondary }}><Bell size={18} /> Notifications</span>
               <input
                 type="checkbox"
                 checked={notify}
