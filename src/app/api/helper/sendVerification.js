d4c857edd332fe5aa064ccd0633fc72a324b0cd1
juwon <asehindej@gmail.com>
2026-01-12 17:50:34 +0100

    Password

--- src/app/api/helper/sendVerification.js
+++ src/app/api/helper/sendVerification.js
@@ -8,17 +8,13 @@ export function sendVerification(receiverEmail, code, link ) {
 
         <p>
           Welcome to <strong>PAX26</strong>.<br/>
-          Use <strong>either</strong> the verification code <strong>or</strong> the button below to activate your account.
+          Use the verification code below to activate your account.
         </p>
 
         <p><strong>Here is your code:</strong></p>
         <h1 style="letter-spacing: 4px;">${code}</h1>
 
         <p>This code expires in <strong>15 minutes</strong>.</p>
-
-        <p style="color:#d9534f; font-weight:bold;">
-          This link is for your personal use.
-        </p>
 
         <p style="color:#d9534f; font-weight:bold;">
         If you did not create a PAX26 account, you can ignore this email.
