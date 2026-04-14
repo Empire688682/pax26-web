1fb5e1a16004fbe2e77ca3fa03a95509b197448a
JayempireDev <asehindej@gmail.com>
2026-04-14 09:46:01 +0100

    fix: update import statement to use consistent syntax in SelectPhoneWrapper component

--- src/components/Wrappers/SelectPhoneWrapper.jsx
+++ src/components/Wrappers/SelectPhoneWrapper.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
 import { useSearchParams, useRouter } from "next/navigation";
 import { useGlobalContext } from "@/components/Context";
 
